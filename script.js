ec18a78108c99be71f61d8a7197aab5a512e2b9a
y7_s <106118090+y7s21@users.noreply.github.com>
2026-05-09 03:29:59 +0300

    updated

--- script.js
+++ script.js
@@ -217,8 +217,7 @@ async function loadGitHubProjects() {
     if (!container) return;
 
     try {
-        const response = await fetch("https://api.github.com/users/y7s21/repos?sort=updated&per_page=100");
-
+            const response = await fetch("https://api.github.com/users/y7s21/repos?sort=updated&per_page=100");
         if (!response.ok) {
             throw new Error("Could not load GitHub repositories");
         }
